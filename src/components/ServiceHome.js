c82b19cc273a14183062149a7950344ba99da315
LamLDT <lamldt_tts@rikkeisoft.com>
2024-01-17 11:37:24 +0700

    BK-4 Design Homepage's Facilities section

---
+++ src/components/ServiceHome.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./ServiceHome.scss"
+
+import SwimmingIcon from '../components/svg/SwimingIcon';
+import RestaurantIcon from '../components/svg/RestaurantIcon';
+import BeachIcon from '../components/svg/BeachIcon';
+import BarIcon from '../components/svg/BarIcon';
+import GymIcon from '../components/svg/GymIcon';
+import ForestIcon from '../components/svg/ForestIcon';
+import GlobeIcon from '../components/svg/GlobeIcon';
+import KidIcon from '../components/svg/KidIcon';
+import SpaIcon from '../components/svg/SpaIcon';
+import ShuttleIcon from '../components/svg/ShuttleIcon';
+import ServiceItem from './ServiceItem';
+
+
+const ServiceHome = () => {
+    return <div className='services'>
+        <div className='intro'>
+            <h1>Meet our top-tier services</h1>
+            <h4>Our team will ensure that every moment during your stay worths it</h4>
+        </div>
+        <div className='menu'>
+            <ServiceItem icon={<SwimmingIcon />} title="Infinity Swimming Pool" />
+            <ServiceItem icon={<SpaIcon />} title="Relaxing Spa" />
+            <ServiceItem icon={<GymIcon />} title="Fitness Center" />
+            <ServiceItem icon={<RestaurantIcon />} title="World-class Restaurant" />
+            <ServiceItem icon={<BarIcon />} title="Stylish Bar" />
+        </div>
+        <div className='menu'>
+            <ServiceItem icon={<BeachIcon />} title="Peaceful and Pristine Beach" />
+            <ServiceItem icon={<ForestIcon />} title="Nature Exploration" />
+            <ServiceItem icon={<GlobeIcon />} title="Cultural Activities" />
+            <ServiceItem icon={<KidIcon/>} title="Kids' Club" />
+            <ServiceItem icon={<ShuttleIcon />} title="Shuttle Services" />
+        </div>
+    </div>
+};
+
+export default ServiceHome;
---
+++ src/components/svg/RestaurantIcon.js
@@ -0,0 +1,10 @@
+function RestaurantIcon() 
+{
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="55" height="40" viewBox="0 -960 960 960" fill="#7C6A46">
+            <path d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z"/>
+        </svg>
+    )
+}
+
+export default RestaurantIcon
