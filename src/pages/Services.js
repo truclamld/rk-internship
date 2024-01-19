07882246ba2e76b1d5376830217873bf74496a0b
LamLDT <lamldt_tts@rikkeisoft.com>
2024-01-19 16:17:59 +0700

    BK-11 add description for services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,6 +6,7 @@ import Restaurant from '../images/Restaurant.jpg';
 import Bar from '../images/Bar.jpg';
 import Spa from '../images/Spa.jpg';
 import Sport from '../images/Sport.jpg';
+import Shuttle from "../images/Shuttle.jpg";
 
 function Services()
 {
@@ -13,6 +14,7 @@ function Services()
     const barDesc = "Located close to the pool and the beach, our bartenders will cater your taste buds with selections of drinks and light meals while you are enjoying the scenery."
     const spaDesc = "Revitalize and re-energize your body and mind with the help of our professional therapists.";
     const sportDesc = "Spice up your vacation with one or multiple activities, whatever you want. Hop on a kayak and sail through the poetic tropical sea, or stroll through the hill and explore the local nature.";
+    const shuttleDesc = "Our shuttle service is always ready to either bring you to or from the airport, or show you around the island with a dedicated tour guide who will also provide the itineraries for your trip."
 
     return(
         <div className='main'>
@@ -21,6 +23,7 @@ function Services()
             <ServiceLayout title="Bars" subtitle={barDesc} image={Bar} direction="row-reverse" />
             <ServiceLayout title="Spa" subtitle={spaDesc} image={Spa} />
             <ServiceLayout title="Sport & Recreation" subtitle={sportDesc} image={Sport} direction="row-reverse" />
+            <ServiceLayout title="Shuttle Services" subtitle={shuttleDesc} image={Shuttle} />
         </div>
     )
 }
