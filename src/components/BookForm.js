4fb9b6cedbe06313bf32ddbb34bd0468521c6fe0
LamLDT <lamldt_tts@rikkeisoft.com>
2024-01-26 17:35:25 +0700

    BK-16-backend-connect

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -39,7 +39,7 @@ function BookForm() {
 
     const Reserve = (value) => {
         console.log(value);
-        redirect("/booking?accomCat="+ value.accomCat + "&accomType="+ value.accomType + "&chkIn="+ value.chkIn + "&chkOut="+ value.chkOut + "&guestNo="+ value.guestNo + "}");
+        //redirect("/booking?accomCat="+ value.accomCat + "&accomType="+ value.accomType + "&chkIn="+ value.chkIn + "&chkOut="+ value.chkOut + "&guestNo="+ value.guestNo + "}");
     }
 
     return (
